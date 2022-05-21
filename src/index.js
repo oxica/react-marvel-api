1bba9d49aee591cb89ba5355ac715a8c664c47c4
Oksana <kskaplun@gmail.com>
2022-05-21 22:01:06 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './components/app/App';
 import MarvelService from 'services/MarvelService';
 import './style/style.scss';
 
+// const marvelService = new MarvelService();
+
+// marvelService.getAllCharacters().then(res => console.log(res));
+
 const marvelService = new MarvelService();
 
 marvelService
