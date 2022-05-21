65588d6ae50bc59c36c38fe98e75aa7a05af3532
Oksana <kskaplun@gmail.com>
2022-05-21 22:10:54 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import App from './components/app/App';
 import MarvelService from 'services/MarvelService';
 import './style/style.scss';
 
-// const marvelService = new MarvelService();
-
-// marvelService.getAllCharacters().then(res => console.log(res));
-
 const marvelService = new MarvelService();
 
 marvelService
